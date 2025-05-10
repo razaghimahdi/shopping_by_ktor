a6c0becc7f0d65f71439ac1d2a577a232bcf3c8c
razaghimahdi <razaghimahdi78@gmail.com>
2025-05-10 16:09:43 +0430

    - Updated CustomException.kt: added new exception

--- src/main/kotlin/com/shoppingbyktor/utils/CustomException.kt
+++ src/main/kotlin/com/shoppingbyktor/utils/CustomException.kt
@@ -3,4 +3,5 @@ package com.shoppingbyktor.utils
 class UserNotExistException : Exception()
 class EmailNotExist : Exception()
 class PasswordNotMatch : Exception()
+class FillInputCorrect : Exception()
 class CommonException(itemName: String) : Exception(itemName)
