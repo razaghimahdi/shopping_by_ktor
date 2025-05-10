b8db2e5fcd209f017324bad00179f5f6ba462621
razaghimahdi <razaghimahdi78@gmail.com>
2025-05-10 16:09:21 +0430

    - Updated projects: removed some unnecessary functions and parameters

--- src/main/kotlin/com/shoppingbyktor/plugins/CongfigureAuth.kt
+++ src/main/kotlin/com/shoppingbyktor/plugins/CongfigureAuth.kt
@@ -8,30 +8,17 @@ import io.ktor.server.auth.jwt.*
 
 fun Application.configureAuth() {
     install(Authentication) {
-        /**
-         * Setup the JWT authentication to be used in [Routing].
-         * If the token is valid, the corresponding [User] is fetched from the database.
-         * The [User] can then be accessed in each [ApplicationCall].
-         */
-        jwt (RoleManagement.CUSTOMER.role){
-            provideJwtAuthConfig(this, RoleManagement.CUSTOMER)
-        }
+
     }
 }
 
-fun provideJwtAuthConfig(jwtConfig: JWTAuthenticationProvider.Config, userRole: RoleManagement) {
+fun provideJwtAuthConfig(jwtConfig: JWTAuthenticationProvider.Config) {
     jwtConfig.verifier(JwtController.verifier)
     jwtConfig.realm = "shoppingbyktor"
     jwtConfig.validate {
         val userId = it.payload.getClaim("userId").asString()
         val email = it.payload.getClaim("email").asString()
         val userType = it.payload.getClaim("userType").asString()
-        if (userType == userRole.role) {
-            JwtTokenRequest(userId, email, userType)
-        } else null
+        JwtTokenRequest(userId, email, userType)
     }
-}
-
-enum class RoleManagement(val role: String) {
-    CUSTOMER("customer")
 }
