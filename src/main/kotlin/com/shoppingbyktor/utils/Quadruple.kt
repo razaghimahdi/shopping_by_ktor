fe12f424cb700bb7d2c91ec8ad37d297303ef82b
razaghimahdi <razaghimahdi78@gmail.com>
2025-05-11 22:29:02 +0430

    - Added Quadruple.kt

---
+++ src/main/kotlin/com/shoppingbyktor/utils/Quadruple.kt
@@ -0,0 +1,16 @@
+package com.shoppingbyktor.utils
+
+data class Quadruple<A, B, C, D>(
+    val first: A,
+    val second: B,
+    val third: C,
+    val fourth: D
+)
+
+data class Quintuple<A, B, C, D, E>(
+    val first: A,
+    val second: B,
+    val third: C,
+    val fourth: D,
+    val fifth: E
+)
