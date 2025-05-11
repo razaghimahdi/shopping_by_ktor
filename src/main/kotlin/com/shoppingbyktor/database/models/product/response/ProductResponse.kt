64a3331dc6fc7033033e4f314d85c9e214c9ea5f
razaghimahdi <razaghimahdi78@gmail.com>
2025-05-11 22:26:54 +0430

    - Updated ProductResponse.kt, JwtTokenRequest.kt

--- src/main/kotlin/com/shoppingbyktor/database/models/product/response/ProductResponse.kt
+++ src/main/kotlin/com/shoppingbyktor/database/models/product/response/ProductResponse.kt
@@ -4,10 +4,14 @@ data class ProductResponse(
     val categoryId: String,
     val title: String,
     val productImage:List<String>,
+    val image:String,
     val description: String,
     val color: String?,
     val size: String?,
     val price: Double,
     val discountPrice: Double?,
-    val quantity: Int
+    val quantity: Int,
+    val likes: Int,
+    val isLike:Boolean,
+    val videoLink: String,
 )
