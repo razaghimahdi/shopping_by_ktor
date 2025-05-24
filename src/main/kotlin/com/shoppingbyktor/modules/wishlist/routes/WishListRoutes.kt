836a33155186c170be27bddac2059efd34047b94
razaghimahdi <razaghimahdi78@gmail.com>
2025-05-24 14:45:45 +0430

    - Updated WishListController.kt

--- src/main/kotlin/com/shoppingbyktor/modules/wishlist/routes/WishListRoutes.kt
+++ src/main/kotlin/com/shoppingbyktor/modules/wishlist/routes/WishListRoutes.kt
@@ -22,78 +22,87 @@ import io.ktor.server.routing.*
  * @param wishlistController The controller responsible for handling wish list operations.
  */
 fun Route.wishListRoutes(wishlistController: WishListController) {
-    route("wishlist") {
+    route("product") {
 
-            /**
-             * POST request to add a product to the user's wish list.
-             *
-             * @param productId The ID of the product to add to the wish list.
-             * @response A response indicating the success of adding the product to the wish list.
-             */
-            post({
-                tags("Wish List")
-                summary = "auth[customer]"
-                request {
-                    body<WisListRequest>()
-                }
-                apiResponse()
-            }) {
-                val requestBody = call.receive<WisListRequest>()
-                call.respond(
-                    ApiResponse.success(
-                        wishlistController.addToWishList(call.currentUser().userId, requestBody.productId),
-                        HttpStatusCode.OK
-                    )
-                )
+        /**
+         * POST request to add a product to the user's wish list.
+         *
+         * @param productId The ID of the product to add to the wish list.
+         * @response A response indicating the success of adding the product to the wish list.
+         */
+        post({
+            tags("Wish List")
+            summary = "auth[customer]"
+            request {
+                body<WisListRequest>()
             }
-
-            /**
-             * GET request to retrieve the user's wish list.
-             *
-             * @param limit The maximum number of products to retrieve from the wish list.
-             * @response A response containing the products in the user's wish list.
-             */
-            get({
-                tags("Wish List")
-                summary = "auth[customer]"
-                request {
-                    queryParameter<String>("limit") {
-                        required = true
-                    }
-                }
-                apiResponse()
-            }) {
-                val (limit) = call.requiredParameters("limit") ?: return@get
-                call.respond(
-                    ApiResponse.success(
-                        wishlistController.getWishList(call.currentUser().userId, limit.toInt()),
-                        HttpStatusCode.OK
-                    )
+            apiResponse()
+        }) {
+            val requestBody = call.receive<WisListRequest>()
+            call.respond(
+                ApiResponse.success(
+                    wishlistController.addToWishList(call.currentUser().userId, requestBody.productId),
+                    HttpStatusCode.OK
                 )
-            }
-
-            /**
-             * DELETE request to remove a product from the user's wish list.
-             *
-             * @param productId The ID of the product to remove from the wish list.
-             * @response A response indicating the success of removing the product from the wish list.
-             */
-            delete({
-                tags("Wish List")
-                summary = "auth[customer]"
-                request {
-                    queryParameter<String>("productId") {
-                        required = true
-                    }
-                }
-                apiResponse()
-            }) {
-                val (productId) = call.requiredParameters("productId") ?: return@delete
-                call.respond(
-                    ApiResponse.success(
-                        wishlistController.removeFromWishList(call.currentUser().userId, productId.toLong()), HttpStatusCode.OK
-                    )
-                )
-            }
+            )
         }
+
+        /**
+         * GET request to retrieve the user's wish list.
+         *
+         * @param limit The maximum number of products to retrieve from the wish list.
+         * @response A response containing the products in the user's wish list.
+         */
+        get("wishlist", {
+            tags("Wish List")
+            summary = "auth[customer]"
+            request {
+                queryParameter<Long>("category_id")
+                queryParameter<Int>("per_page")
+                queryParameter<Int>("page")
+            }
+            apiResponse()
+        }) {
+            val categoryId = call.parameters["category_id"]?.toLongOrNull()
+            val (page, perPage) = call.requiredParameters("page", "per_page") ?: return@get
+            val userId = call.currentUser().userId
+
+            call.respond(
+                ApiResponse.success(
+                    wishlistController.getWishList(
+                        userId = userId,
+                        categoryId = categoryId,
+                        page = page.toInt(),
+                        perPage = perPage.toInt()
+                    ),
+                    HttpStatusCode.OK
+                )
+            )
+        }
+
+        /**
+         * DELETE request to remove a product from the user's wish list.
+         *
+         * @param productId The ID of the product to remove from the wish list.
+         * @response A response indicating the success of removing the product from the wish list.
+         */
+        delete({
+            tags("Wish List")
+            summary = "auth[customer]"
+            request {
+                queryParameter<String>("productId") {
+                    required = true
+                }
+            }
+            apiResponse()
+        }) {
+            val (productId) = call.requiredParameters("productId") ?: return@delete
+            call.respond(
+                ApiResponse.success(
+                    wishlistController.removeFromWishList(call.currentUser().userId, productId.toLong()),
+                    HttpStatusCode.OK
+                )
+            )
+        }
+    }
 }
