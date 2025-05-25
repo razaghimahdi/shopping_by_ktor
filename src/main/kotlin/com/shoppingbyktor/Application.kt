59f266b919953550db60037437da574d23eb964d
razaghimahdi <razaghimahdi78@gmail.com>
2025-05-25 17:46:20 +0430

    - Updated Application.kt

--- src/main/kotlin/com/shoppingbyktor/Application.kt
+++ src/main/kotlin/com/shoppingbyktor/Application.kt
@@ -6,11 +6,14 @@ import com.typesafe.config.ConfigFactory
 import io.ktor.server.config.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.slf4j.LoggerFactory
 
 fun main() {
     val config = HoconApplicationConfig(ConfigFactory.load("application.conf"))
     val port = config.property("ktor.deployment.port").getString().toInt()
     val host = config.property("ktor.deployment.host").getString()
+    val logger = LoggerFactory.getLogger("MyAppLogger")
+    logger.info("main")
     embeddedServer(Netty, port = port, host = host) {
         configureDataBase()
         configureBasic()
