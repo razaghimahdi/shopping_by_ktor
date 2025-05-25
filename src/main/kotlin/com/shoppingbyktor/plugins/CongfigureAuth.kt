42967e1ae7ac7341ad3a9ad9c07f8d770774e327
razaghimahdi <razaghimahdi78@gmail.com>
2025-05-25 17:46:05 +0430

    - Updated configureAuth.kt

--- src/main/kotlin/com/shoppingbyktor/plugins/CongfigureAuth.kt
+++ src/main/kotlin/com/shoppingbyktor/plugins/CongfigureAuth.kt
@@ -5,22 +5,30 @@ import com.shoppingbyktor.database.models.user.body.JwtTokenRequest
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import org.slf4j.LoggerFactory
 
 fun Application.configureAuth() {
     install(Authentication) {
 
-        jwt {
+        jwt("jwt") {
             provideJwtAuthConfig(this)
         }
     }
 }
 
 fun provideJwtAuthConfig(jwtConfig: JWTAuthenticationProvider.Config) {
+    val logger = LoggerFactory.getLogger("MyAppLogger")
+    logger.info("provideJwtAuthConfig jwtConfig realm: "+jwtConfig.realm)
+    logger.info("provideJwtAuthConfig jwtConfig name: "+jwtConfig.name)
+
     jwtConfig.verifier(JwtController.verifier)
     jwtConfig.realm = "shoppingbyktor"
     jwtConfig.validate {
+        logger.info("provideJwtAuthConfig jwtConfig.validate: " )
         val userId = it.payload.getClaim("userId").asLong()
         val email = it.payload.getClaim("email").asString()
+        logger.info("Token claims: userId=$userId, email=$email")
+
         JwtTokenRequest(userId, email)
     }
 }
