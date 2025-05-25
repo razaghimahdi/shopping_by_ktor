d1b21d00352e875341a2f92fb16ce9ef190383a6
razaghimahdi <razaghimahdi78@gmail.com>
2025-05-25 17:45:18 +0430

    - Updated WisListRequest.kt

--- src/main/kotlin/com/shoppingbyktor/database/models/WisListRequest.kt
+++ src/main/kotlin/com/shoppingbyktor/database/models/WisListRequest.kt
@@ -4,7 +4,7 @@ import org.valiktor.functions.isNotNull
 import org.valiktor.functions.isNotZero
 import org.valiktor.validate
 
-data class WisListRequest(val productId: Long) {
+data class WisListRequest(val productId: Long?) {
     fun validation() {
         validate(this) {
             validate(WisListRequest::productId).isNotNull().isNotZero()
