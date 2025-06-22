38a3678ed8ede927b309056c97090737a2927113
razaghimahdi <razaghimahdi78@gmail.com>
2025-06-22 20:30:15 +0430

    - Updated ProfileRoutes.kt: fixed major bugs

--- src/main/kotlin/com/shoppingbyktor/modules/profile/routes/ProfileRoutes.kt
+++ src/main/kotlin/com/shoppingbyktor/modules/profile/routes/ProfileRoutes.kt
@@ -28,16 +28,17 @@ import java.util.*
  * @param userProfileController The controller responsible for handling user profile-related operations.
  */
 fun Route.profileRoutes(userProfileController: ProfileController) {
-        route("profile") {
+    route("profile") {
 
-            /**
-             * GET request to retrieve the profile of the current user.
-             *
-             * @response A response containing the profile information of the user.
-             */
+        /**
+         * GET request to retrieve the profile of the current user.
+         *
+         * @response A response containing the profile information of the user.
+         */
+        authenticate("jwt") {
             get({
                 tags("Profile")
-                summary = "auth[admin, seller, customer]"
+                summary = "auth[customer]"
                 apiResponse()
             }) {
                 call.respond(
@@ -46,112 +47,105 @@ fun Route.profileRoutes(userProfileController: ProfileController) {
                     )
                 )
             }
-
-            /**
-             * PUT request to update the profile of the current user.
-             *
-             * @param firstName The user's first name.
-             * @param lastName The user's last name.
-             * @param mobile The user's secondary mobile number.
-             * @param faxNumber The user's fax number.
-             * @param streetAddress The user's street address.
-             * @param city The user's city.
-             * @param identificationType The type of identification provided.
-             * @param identificationNo The identification number.
-             * @param occupation The user's occupation.
-             * @param postCode The user's postal code.
-             * @param gender The user's gender.
-             * @response A response indicating the success of the profile update.
-             */
-            put({
-                tags("Profile")
-                summary = "auth[admin, seller, customer]"
-                request {
-                    queryParameter<String>("firstName")
-                    queryParameter<String>("lastName")
-                    queryParameter<String>("mobile")
-                    queryParameter<String>("faxNumber")
-                    queryParameter<String>("streetAddress")
-                    queryParameter<String>("city")
-                    queryParameter<String>("identificationType")
-                    queryParameter<String>("identificationNo")
-                    queryParameter<String>("occupation")
-                    queryParameter<String>("postCode")
-                    queryParameter<String>("gender")
-                }
-                apiResponse()
-            }) {
-                val params = UserProfileRequest(
-                    firstName = call.parameters["firstName"],
-                    lastName = call.parameters["lastName"],
-                    mobile = call.parameters["mobile"],
-                    faxNumber = call.parameters["faxNumber"],
-                    streetAddress = call.parameters["streetAddress"],
-                    city = call.parameters["city"],
-                    identificationType = call.parameters["identificationType"],
-                    identificationNo = call.parameters["identificationNo"],
-                    occupation = call.parameters["occupation"],
-                    postCode = call.parameters["postCode"],
-                    gender = call.parameters["gender"],
-                )
-                call.respond(
-                    ApiResponse.success(
-                        userProfileController.updateProfile(call.currentUser().userId, params), HttpStatusCode.OK
-                    )
-                )
-            }
-
-            /**
-             * POST request to upload a new profile image for the user.
-             *
-             * @param image The image file to be uploaded.
-             * @response A response containing the file name of the uploaded image.
-             */
-            post("image-upload", {
-                tags("Profile")
-                summary = "auth[admin, seller, customer]"
-                request {
-                    multipartBody {
-                        mediaTypes = setOf(ContentType.MultiPart.FormData)
-                        part<File>("image") {
-                            mediaTypes = setOf(
-                                ContentType.Image.PNG, ContentType.Image.JPEG, ContentType.Image.SVG
-                            )
-                        }
-                    }
-
-                }
-                apiResponse()
-            }) {
-                val multipartData = call.receiveMultipart()
-
-                multipartData.forEachPart { part ->
-                    when (part) {
-                        is PartData.FileItem -> {
-                            UUID.randomUUID()?.let { imageId ->
-                                val fileName = part.originalFileName as String
-                                val fileLocation = fileName.let {
-                                    "${AppConstants.ImageFolder.PROFILE_IMAGE_LOCATION}$imageId${it.fileExtension()}"
-                                }
-                                fileLocation.let {
-                                    File(it).writeBytes(withContext(Dispatchers.IO) {
-                                        part.streamProvider().readBytes()
-                                    })
-                                }
-                                val fileNameInServer = imageId.toString().plus(fileLocation.fileExtension())
-                                userProfileController.updateProfileImage(call.currentUser().userId, fileNameInServer)
-                                    .let {
-                                        call.respond(
-                                            ApiResponse.success(fileNameInServer, HttpStatusCode.OK)
-                                        )
-                                    }
-                            }
-                        }
-
-                        else -> {}
-                    }
-                    part.dispose()
-                }
-            }
         }
+
+        /**
+         * PUT request to update the profile of the current user.
+         *
+         * @param firstName The user's first name.
+         * @param lastName The user's last name.
+         * @param mobile The user's secondary mobile number.
+         * @param faxNumber The user's fax number.
+         * @param streetAddress The user's street address.
+         * @param city The user's city.
+         * @param identificationType The type of identification provided.
+         * @param identificationNo The identification number.
+         * @param occupation The user's occupation.
+         * @param postCode The user's postal code.
+         * @param gender The user's gender.
+         * @response A response indicating the success of the profile update.
+         */
+        put({
+            tags("Profile")
+            summary = "auth[customer]"
+            request {
+                queryParameter<String>("name")
+                queryParameter<Int>("age")
+            }
+            apiResponse()
+        }) {
+            val params = UserProfileRequest(
+                name = call.parameters["name"],
+                age = call.parameters["age"]?.toIntOrNull(),
+            )
+            call.respond(
+                ApiResponse.success(
+                    userProfileController.updateProfile(call.currentUser().userId, params), HttpStatusCode.OK
+                )
+            )
+        }
+
+        /**
+         * POST request to upload a new profile image for the user.
+         *
+         * @param image The image file to be uploaded.
+         * @response A response containing the file name of the uploaded image.
+         */
+        post({
+            tags("Profile")
+            summary = "auth[customer]"
+            request {
+                multipartBody {
+                    mediaTypes = setOf(ContentType.MultiPart.FormData)
+                    part<File>("image") {
+                        mediaTypes = setOf(
+                            ContentType.Image.PNG, ContentType.Image.JPEG, ContentType.Image.SVG
+                        )
+                    }
+                    part<String>("name")
+                    part<String>("age")
+                }
+            }
+            apiResponse()
+        }) {
+            val multipartData = call.receiveMultipart()
+            var name: String? = null
+            var age: Int? = null
+            var imageFileNameInServer: String? = null
+
+            multipartData.forEachPart { part ->
+                when (part) {
+                    is PartData.FormItem -> {
+                        when (part.name) {
+                            "name" -> name = part.value
+                            "age" -> age = part.value.toIntOrNull()
+                        }
+                    }
+
+                    is PartData.FileItem -> {
+                        val fileName = part.originalFileName ?: "image"
+                        val imageId = UUID.randomUUID()
+                        val fileExtension = fileName.fileExtension()
+                        val serverFileName = "$imageId$fileExtension"
+                        val filePath = "${AppConstants.ImageFolder.PROFILE_IMAGE_LOCATION}$serverFileName"
+
+                        withContext(Dispatchers.IO) {
+                            File(filePath).writeBytes(part.streamProvider().readBytes())
+                        }
+                        imageFileNameInServer = serverFileName
+                    }
+
+                    else -> {}
+                }
+                part.dispose()
+            }
+
+
+            val userId = call.currentUser().userId
+            val updatedProfile = userProfileController.updateProfileImage(userId, imageFileNameInServer, name, age)
+            call.respond(ApiResponse.success(updatedProfile, HttpStatusCode.OK))
+        }
+
+
+    }
 }
