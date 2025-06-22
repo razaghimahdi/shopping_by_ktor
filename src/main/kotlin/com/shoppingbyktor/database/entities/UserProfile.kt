450caf40c450fa41371192842a3ebca2dc04827c
razaghimahdi <razaghimahdi78@gmail.com>
2025-06-22 20:22:35 +0430

    - Updated UserProfile.kt: fixed some parameters

--- src/main/kotlin/com/shoppingbyktor/database/entities/UserProfile.kt
+++ src/main/kotlin/com/shoppingbyktor/database/entities/UserProfile.kt
@@ -8,18 +8,10 @@ import org.jetbrains.exposed.dao.id.EntityID
 
 object UserProfileTable : BaseIntIdTable("user_profile") {
     val userId = reference("user_id", UserTable.id)
-    val image = text("image").nullable()
-    val firstName = text("first_name").nullable()
-    val lastName = text("last_name").nullable()
-    val mobile = text("mobile").nullable()
-    val faxNumber = text("fax_number").nullable()
-    val streetAddress = text("street_address").nullable()
-    val city = text("city").nullable()
-    val identificationType = text("identification_type").nullable()
-    val identificationNo = text("identification_no").nullable()
-    val occupation = text("occupation").nullable()
-    val postCode = text("post_code").nullable()
-    val gender = text("gender").nullable()
+    val image = text("image").default("")
+    val name = text("name").default("")
+    val email = text("email").default("")
+    val age = integer("age").default(0)
 }
 
 class UsersProfileDAO(id: EntityID<Long>) : BaseIntEntity(id, UserProfileTable) {
@@ -27,47 +19,23 @@ class UsersProfileDAO(id: EntityID<Long>) : BaseIntEntity(id, UserProfileTable) 
 
     var userId by UserProfileTable.userId
     var image by UserProfileTable.image
-    var firstName by UserProfileTable.firstName
-    var lastName by UserProfileTable.lastName
-    var mobile by UserProfileTable.mobile
-    var faxNumber by UserProfileTable.faxNumber
-    var streetAddress by UserProfileTable.streetAddress
-    var city by UserProfileTable.city
-    var identificationType by UserProfileTable.identificationType
-    var identificationNo by UserProfileTable.identificationNo
-    var occupation by UserProfileTable.occupation
-    var postCode by UserProfileTable.postCode
-    var gender by UserProfileTable.gender
+    var age by UserProfileTable.age
+    var name by UserProfileTable.name
+    var email by UserProfileTable.email
     fun response() = UserProfile(
         userId.value,
         image,
-        firstName,
-        lastName,
-        mobile,
-        faxNumber,
-        streetAddress,
-        city,
-        identificationType,
-        identificationNo,
-        occupation,
-        postCode,
-        gender,
+        age,
+        name,
+        email
     )
 }
 
 data class UserProfile(
     var userId: Long,
-    val image: String?,
-    val firstName: String?,
-    val lastName: String?,
-    val mobile: String?,
-    val faxNumber: String?,
-    val streetAddress: String?,
-    val city: String?,
-    val identificationType: String?,
-    val identificationNo: String?,
-    val occupation: String?,
-    val postCode: String?,
-    val gender: String?
+    val profile_url: String,
+    val age: Int,
+    val name: String,
+    val email: String,
 )
 
